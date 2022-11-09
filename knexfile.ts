b73e505715bab885323d3798b67b1821be79661a
Kuramoto <kazuma.kuramoto@yuyosoft.com>
2022-11-09 11:18:44 -0800

    connect to postgres

---
+++ knexfile.ts
@@ -0,0 +1,8 @@
+export default{
+  client:'pg',
+  connection:process.env.DATABASE_URL,
+  migrations:{
+    extension:'ts',
+  },
+}
+
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,36 @@
+import { PrismaClient } from '@prisma/client';
 import express from 'express'
 import morgan from 'morgan';
+import { nanoid } from 'nanoid';
+
+const db=new PrismaClient({log:['error','info','query','warn']});
+const genId=()=>nanoid(16);
+
+const seedDatabase =async()=>{
+  if((await db.post.count())===0){
+    await db.post.createMany({
+      data:[{
+        id: genId(),
+        slug:'node-typescript-docker',
+        title:'Node Typescript Docker',
+        publishedAt:new Date()
+      },{
+        id:genId(),
+        slug:'Draft post',
+        title:'Draft post',
+      }]
+    })
+  }
+}
+
+seedDatabase()
 
 const app = express();
 app.use(morgan('dev'));
 
-app.get('/',(req,res)=>{
-  res.json({hello:'world'})
+app.get('/',async(req,res)=>{
+  const posts = await db.post.findMany();
+  res.json(posts);
 })
 
 const port =Number(process.env.PORT ?? 8080);
