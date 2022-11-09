98bf53b7847a2eaf015f39ae091ff1c5c0705e51
Kuramoto <kazuma.kuramoto@yuyosoft.com>
2022-11-09 09:09:53 -0800

    first commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express'
+import morgan from 'morgan';
+
+const app = express();
+app.use(morgan('dev'));
+
+app.get('/',(req,res)=>{
+  res.json({hello:'world'})
+})
+
+const port =Number(process.env.PORT ?? 8080);
+app.listen(port,'0.0.0.0',()=>{
+  console.log(`Server started at http://localhost:${port}`)
+})
