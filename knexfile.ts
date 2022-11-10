2204e8055cb22c4a1b47b8d56521b01d9e9affa9
Kuramoto <kazuma.kuramoto@yuyosoft.com>
2022-11-10 11:07:59 -0800

    save

--- knexfile.ts
+++ knexfile.ts
@@ -1,6 +1,12 @@
 export default{
-  client:'pg',
-  connection:process.env.DATABASE_URL,
+  client:'mysql',
+  connection: {
+    host : '127.0.0.1',
+    port : 3306,
+    user : 'root',
+    password : 'password',
+    database : 'pitchtrainer_test'
+  },
   migrations:{
     extension:'ts',
   },
